f7165d36a8830a5e1f6d1ef5c2deeea11b6bb25a
Nursurtan <arapovnur23@gmail.com>
2021-10-11 21:17:26 +0700

    Used import AVFoundation and added sound

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var audioPlayer:AVAudioPlayer?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
         
     @IBOutlet weak var ballImageView: UIImageView!
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        // Getting the url
+        let url = Bundle.main.url(forResource: "Shaking 3", withExtension: "wav")
+        // Make sure that we've got the url, otherwise abord
+        guard url != nil else {
+            return
+        }
+        // Create the audio player the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
         
         let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball4")]
         
